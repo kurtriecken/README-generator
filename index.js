a002a51a3956d5ee02330b3165a0e9134653e9a4
Kurt Riecken <kurt.riecken@gmail.com>
2023-10-19 14:26:33 -0400

    added questions array

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your name?',
+    'What is the title of your project?',
+    'Enter a description of your project:',
+    'How is this project installed?',
+    'How will this project be used?',
+    'Please share how others may contribute to this project.'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
