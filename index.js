c7db7415db2d4cca9285bdc4f48ce3b6a391b4ec
Kurt Riecken <kurt.riecken@gmail.com>
2023-10-19 14:37:17 -0400

    installed node packages; questions working

--- index.js
+++ index.js
@@ -5,19 +5,75 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    'What is your name?',
-    'What is the title of your project?',
-    'Enter a description of your project:',
-    'How is this project installed?',
-    'How will this project be used?',
-    'Please share how others may contribute to this project.'
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'How is this project installed?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How will this project be used?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please share how others may contribute to this project.', 
+        name: 'contributions',
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            console.log(response);
+        })
+}
 
 // Function call to initialize app
 init();
+
+
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: 'What is your user name?',
+//       name: 'username',
+//     },
+//     {
+//       type: 'password',
+//       message: 'What is your password?',
+//       name: 'password',
+//     },
+//     {
+//       type: 'password',
+//       message: 'Re-enter password to confirm:',
+//       name: 'confirm',
+//     },
+//   ])
+//   .then((response) =>
+//     response.confirm === response.password
+//       ? console.log('Success!')
+//       : console.log('You forgot your password already?!')
+//   );
